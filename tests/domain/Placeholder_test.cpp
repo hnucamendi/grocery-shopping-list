221a7211c67430e166113b761c929bae049644ac
Harold Arriola Nucamendi <55587068+hnucamendi@users.noreply.github.com>
2026-02-14 16:53:36 -0500

    add specific mainframe test

--- tests/domain/Placeholder_test.cpp
+++ tests/domain/Placeholder_test.cpp
@@ -1,7 +1,5 @@
-#include "ui/MainFrame.h"
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("Grocery Shopping List Frame loads window") {
-    MainFrame frame;
-  REQUIRE(frame.GetTitle() == wxString("Grocery Shopping List"));
+TEST_CASE("Placeholder test") {
+  REQUIRE(true);
 }
